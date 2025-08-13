9f03e25be14e15a95689b612ff7ebd6a0da5ec3b
Vinzenco-alpha <santanuhui7@gmail.com>
2025-08-13 15:56:48 +0530

    Process of live page

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // Replace with your GitHub username and repo name:
-const repoName = 'Portfolio-vite'
+
 
 export default defineConfig({
   plugins: [react()],
-  base: `/${repoName}/`,
+  base: '/Portfolio/',
 })
